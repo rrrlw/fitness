e68f453680d54f972cf62ebbfd314a4edbfc3c26
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-01-06 10:25:49 -0500

    Added package description/documentation

---
+++ R/fitness.R
@@ -0,0 +1,9 @@
+#' Implementing Functionality for Fitness Landscapes
+#'
+#' A helpful library for useRs studying fitness landscapes. `fitness`
+#' implements popular models for fitness landscapes (e.g. NK model,
+#' rough Mt. Fuji) and associated functionality (e.g. optima identification).
+#'
+#' @name fitness
+#' @docType package
+NULL
